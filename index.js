31c918bcac01646af404ba3be9bd99a8871fc120
Tushar12152 <tusharimran470@gmail.com>
2023-11-24 18:35:27 +0600

    users post in db and get users from db

--- index.js
+++ index.js
@@ -26,13 +26,45 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
+
+    const usersCollection = client.db("MealMasterDb").collection("users");
+    
+
+
+
+
+
+    //users collections operation
+     app.post('/users',async(req,res)=>{
+           const user=req.body;
+           
+           const result=await usersCollection.insertOne(user)
+           res.send(result)
+     })
+
+     app.get('/users/:email',async(req,res)=>{
+          const  email=req.params.email;
+        //    console.log(email);
+           const query={email:email}
+           const result= await usersCollection.findOne(query)
+           res.send(result)
+     })
+
+
+
+
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
