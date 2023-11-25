608e796a381d1c54621d3321d3e892e969f386fe
Tushar12152 <tusharimran470@gmail.com>
2023-11-25 08:02:43 +0600

    single meal get from db by id

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tgzt8q2.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -67,6 +67,13 @@ async function run() {
          res.send(result)
      })
 
+     app.get('/meals/:id',async(req,res)=>{
+          const id=req.params.id
+          const query={_id:new ObjectId(id)}
+
+          const result= await mealCollection.findOne(query)
+          res.send(result)
+     })
 
 
 
